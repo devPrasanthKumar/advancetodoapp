cbfbc2f1944b95d528fbf54cc5ac65ece8107d00
devPrasanthKumar <prok52010get@gmail.com>
2023-10-16 09:26:23 -0700

    fisrt commit 10

--- adtodoapp/settings.py
+++ adtodoapp/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "@neaux^*!@z5q%&^=^#+-)s8vb3xni^+qyp)@jg^lk=us-zgc9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 
 
