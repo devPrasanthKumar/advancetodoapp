fd01efa6c75e359fcafbd1a0834328ad2e46112f
devPrasanthKumar <prok52010get@gmail.com>
2023-10-16 08:54:23 -0700

    fisrt commit 8

--- adtodoapp/settings.py
+++ adtodoapp/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -120,7 +121,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [os.path.join(BASE_DIR / 'static')]
-
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # media
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
